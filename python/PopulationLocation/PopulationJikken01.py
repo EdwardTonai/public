6e70531c3921c40c449f61e1e9e335e2d1e1b2ac
EdwardTonai <Edward@Tonai.net>
2015-06-28 01:25:15 -0700

    Updated Canada Population project

--- python/PopulationLocation/PopulationJikken01.py
+++ python/PopulationLocation/PopulationJikken01.py
@@ -429,6 +429,7 @@ def GetCoordinatesSelenium():
 # What are my thoughts on the previous function?
 # What about the getting coordinates?  Did I try BeautifulSoup?
     
+
 # Assuming you are already on the census division page, look for the coordinates
 # Good example:  Toronto - http://en.wikipedia.org/wiki/Toronto
 # Fail example:  Montreal - http://en.wikipedia.org/wiki/Urban_agglomeration_of_Montreal
@@ -562,17 +563,197 @@ def GetCoordinatesSoup():
     print errorRows
 
 
+# Use BeautifulSoup to get Coordinates from wikipedia
+def GetCanadaCensusInfoFromWikipedia(outFile):
+    # Get the information on the census divisions:
+    # Name, Province, Population, Illustrative
+    # If there is no illustrative, fill it with Name
+    
+    br = mechanize.Browser()
 
-# 
-def GetCoordinatesFromWikipediaPage(br, url):
-    print "Getting location coordinates:"
-    print url
+    url = "http://en.wikipedia.org/wiki/List_of_census_divisions_of_Canada_by_population"
     response = br.open(url)
-    print "X1"
     soup = BeautifulSoup(br.response().read())
-    print "X2"
+    
+    table = soup.find_all('tr')
+
+    print "Looking at the table:"
+    rowNum = 0
+    locations = []
+    errorRows = []
+    for row in table:
+        print "Evaluating row: ", rowNum
+        print "-----------------------"
+        columns = row.find_all('td')
+        try:
+            # Test that the first column is a number, incidating it was used in 2011 census
+            int(columns[0].find('span','sorttext').text)
+            # Column 2 - Census Division 
+            name = columns[2].find('span','sorttext').text.encode('utf-8')
+            print name
+            print "*** Getting href to the census division's wikipedia page"
+            locURL = columns[2].find('span','sorttext')
+            locURL = locURL.a['href'].encode('utf-8')
+            locURL = urljoin(url,locURL)
+            print locURL
+
+            # Get the lat/lon coordinates from the census division page
+            latlon = GetCoordinatesFromWikipediaPage(br,locURL)
+
+            # Column 4 - Province (abbreviated)
+            province = columns[4].find('span','sorttext').text.encode('utf-8')
+
+            # Column 5 - Population from 2011 census
+            population = int(columns[5].text.replace(',',''))
+
+            # Column 8 - Illustrative census subdivision
+            illustrative = columns[8].text.encode('utf-8')
+            illustrativeURL = ''
+
+            if illustrative != '':
+                illustrativeURL = columns[8]
+                #print illustrativeURL
+                illustrativeURL = illustrativeURL.a['href'].encode('utf-8')
+                illustrativeURL = urljoin(url, illustrativeURL)
+                print "*** Getting href to the census illustrative division's wikipedia page"
+                print illustrativeURL
+                # If we couldn't retrieve the latlon from the division's page, try the subdivision's page
+                if latlon is None:
+                    print "**** Calculating latlon from Illustrative"
+                    latlon = GetCoordinatesFromWikipediaPage(br,illustrativeURL)
+            else:
+                print "NO ILLUSTRATIVE"
+
+            #print latlon
+            if latlon is None:
+                print "!!!!! NO COORDINATES !!!!!!"
+                latlon = None,None
+
+            # Create a list of the data we used to calculate the population and location
+            thisLocation = [name, province, population, illustrative,latlon[0],latlon[1]]
+            # Append this division's list to the master list of all divisions
+            locations.append(thisLocation)
+            print "*** This location"
+            print thisLocation
+
+        except ValueError,AttributeError:
+            None
+        except:
+            None
+
+        print "======================="
+        rowNum += 1
+
+    # Write all locations to the outfile
+    with open(outFile, "wb") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for line in locations:
+            writer.writerow(line)
+    print "Printing Error Rows"
+    print errorRows
+
+
+
+
+# Use BeautifulSoup to get Coordinates from wikipedia
+def GetCoordinatesNoEncode(outFile):
+    # Get the information on the census divisions:
+    # Name, Province, Population, Illustrative
+    # If there is no illustrative, fill it with Name
+    
+    br = mechanize.Browser()
+
+    url = "http://en.wikipedia.org/wiki/List_of_census_divisions_of_Canada_by_population"
+    response = br.open(url)
+    soup = BeautifulSoup(br.response().read())
+    
+    table = soup.find_all('tr')
+
+    print "Looking at the table:"
+    rowNum = 0
+    locations = []
+    errorRows = []
+    for row in table:
+        print "Evaluating row: ", rowNum
+        #print row.text
+        print "-----------------------"
+        columns = row.find_all('td')
+        try:
+            # Test that the first column is a number, incidating it was used in 2011 census
+            int(columns[0].find('span','sorttext').text)
+
+            # Column 2 - Census Division 
+            name = columns[2].find('span','sorttext').text
+            print name
+            print "*** Getting href to the census division's wikipedia page"
+            locURL = columns[2].find('span','sorttext')
+            locURL = locURL.a['href'].encode('utf-8')
+            locURL = urljoin(url,locURL)
+            print locURL
+
+            # Get the lat/lon coordinates from the census division page
+            latlon = GetCoordinatesFromWikipediaPage(br,locURL)
+
+            # Column 4 - Province (abbreviated)
+            province = columns[4].find('span','sorttext').text
+
+            # Column 5 - Population from 2011 census
+            population = int(columns[5].text.replace(',',''))
+
+            # Column 8 - Illustrative census subdivision
+            illustrative = columns[8].text
+            illustrativeURL = ''
+
+            if illustrative != '':
+                illustrativeURL = columns[8]
+                #print illustrativeURL
+                illustrativeURL = illustrativeURL.a['href']
+                illustrativeURL = urljoin(url, illustrativeURL)
+                print "*** Getting href to the census illustrative division's wikipedia page"
+                print illustrativeURL
+                # If we couldn't retrieve the latlon from the division's page, try the subdivision's page
+                if latlon is None:
+                    print "**** Calculating latlon from Illustrative"
+                    latlon = GetCoordinatesFromWikipediaPage(br,illustrativeURL)
+            else:
+                print "NO ILLUSTRATIVE"
+
+            #print latlon
+            if latlon is None:
+                print "!!!!! NO COORDINATES !!!!!!"
+                latlon = None,None
+
+            # Create a list of the data we used to calculate the population and location
+            thisLocation = [name, province, population, illustrative,latlon[0],latlon[1]]
+            # Append this division's list to the master list of all divisions
+            locations.append(thisLocation)
+            print "*** This location"
+            print thisLocation
+
+        except ValueError,AttributeError:
+            None
+        except:
+            None
+
+        print "======================="
+        rowNum += 1
+
+    # Write all locations to the outfile
+    with open(outFile, "wb") as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        for line in locations:
+            print line
+            writer.writerow(line)
+    print "Printing Error Rows"
+    print errorRows
+
+
+
+# Get the lat/lon coordinates of a municipality from the city's wikipedia page
+def GetCoordinatesFromWikipediaPage(br, url):
+    response = br.open(url)
+    soup = BeautifulSoup(br.response().read())
     latlon = soup.find(class_="geo")
-    print "X3"
     if latlon is not None:
         return tuple([float(x) for x in latlon.text.split(';')])
         print latlon.text
@@ -594,6 +775,20 @@ def FindNorthAndSouthPopulations(lat):
     print "North = ", north
     print "South = ", south
     return north, south
+
+def FindPercentageOfCanadaPopulation(latitude):
+    populations = FindNorthAndSouthPopulations(latitude)
+    totalPopulation = populations[0] + populations[1]
+    percentNorth = float(populations[0]) / totalPopulation * 100
+    percentSouth = float(populations[1]) / totalPopulation * 100
+    print "Total population = ", totalPopulation
+    print "Population living north of latitude {0:.5f} is {1}.".format(latitude, populations[0])
+    print "Population living south of latitude {0:.5f} is {1}.".format(latitude, populations[1])
+    print "{0:.2f}% of Canada's population lives north of latitude {1:.5f}!".format(percentNorth, latitude)
+    print "{0:.2f}% of Canada's population lives south of latitude {1:.5f}!".format(percentSouth, latitude)
+    
+    #print "Population living south of %d is %d." % (latitude, populations[1])
+
 
 br = mechanize.Browser()
 testMontreal = 'http://en.wikipedia.org/wiki/Montr%C3%A9al_(region)'
